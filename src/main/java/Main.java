6c65495f4374a1ac93c7a1400073103c99001f0e
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2021-10-14 00:53:42 +0300

    add sonar configuration

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start SolarCloud usage!");
+        System.out.println("Start Solarcloud usage!");
     }
 }
