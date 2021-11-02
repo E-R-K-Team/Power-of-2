83ab00efb85918fe5aeab52a21eb2e69ea966743
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2021-11-02 19:41:07 +0300

    fix package structure

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,7 +1,6 @@
-
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
     private JButton button = new JButton("Start Game");
