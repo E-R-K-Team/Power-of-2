a6d188dcf3a26c2fb119450c3d6b596079460d2b
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2021-11-03 00:52:41 +0300

    additional class Tile was removed from class Board first spawnTile method was replaced into constructor getBoard method added Unit-test logic added.

--- src/main/java/GameConstants.java
+++ src/main/java/GameConstants.java
@@ -1,10 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class GameConstants {
-    public final static int MIN_BOARD_SIZE = 4;
-    public final static int MAX_BOARD_SIZE = 6;
-    public final static float CHANCE_TO_SPAWN_FOUR = 0.1f;
-    public final static char SLIDE_UP_CHAR = 'w';
-    public final static char SLIDE_DOWN_CHAR = 's';
-    public final static char SLIDE_RIGHT_CHAR = 'd';
-    public final static char SLIDE_LEFT_CHAR = 'a';
-    public final static int WIN_TILE_VALUE = 4096;
+    public static final int MIN_BOARD_SIZE = 4;
+    public static final int MAX_BOARD_SIZE = 6;
+    public static final float CHANCE_TO_SPAWN_FOUR = 0.1f;
+    public static final char SLIDE_UP_CHAR = 'w';
+    public static final char SLIDE_DOWN_CHAR = 's';
+    public static final char SLIDE_RIGHT_CHAR = 'd';
+    public static final char SLIDE_LEFT_CHAR = 'a';
+    public static final int WIN_TILE_VALUE = 2048;
+    public static final List<Character> CONTROL_BUTTONS = Arrays.asList(SLIDE_UP_CHAR, SLIDE_DOWN_CHAR, SLIDE_LEFT_CHAR, SLIDE_RIGHT_CHAR);
 }
