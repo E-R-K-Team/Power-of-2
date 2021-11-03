6209ffa61dfd216226a3c0a7c1c33f558534ac75
RomanRedkovskiy <romanredkovskiy10@gmail.com>
2021-11-03 21:19:55 +0300

    change BoardTest class

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -18,13 +18,13 @@ public class BoardTest {
 
     private void setValuesToTiles(int[] inputValues, Board board) {
         for (int i = 0; i < inputValues.length; i++) {
-            board.getBoard().get(i).value = inputValues[i];
+            board.board.get(i).value = inputValues[i];
         }
     }
 
     private boolean checkValues(int[] expectedValues, Board board) {
         for (int i = 0; i < expectedValues.length; i++) {
-            if (expectedValues[i] != board.getBoard().get(i).value) {
+            if (expectedValues[i] != board.board.get(i).value) {
                 return false;
             }
         }
