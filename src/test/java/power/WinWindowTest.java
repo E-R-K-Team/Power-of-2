a34a6f18a3d75a539315608ce3cb60935726550e
CmakKir <cmak7545@mail.ru>
2021-11-18 12:07:53 +0300

    win tests added

--- src/test/java/power/WinWindowTest.java
+++ src/test/java/power/WinWindowTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WinWindowTest {
 
-//    @Test
-//    void constructorTest(){
-//        WinWindow window = new WinWindow();
-//
-//        assertEquals(window.winText.getText(),"          \u265A You WIN \u265A");
-//    }
+    @Test
+    void constructorTest(){
+        WinWindow window = new WinWindow();
+
+      assertEquals(window.winText.getText(),"          \u265A You WIN \u265A");
+    }
 
 }
